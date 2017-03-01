6a32391d65ea2e1b2edb3d9c7e19d73b63177408
gdw <879382023@qq.com>
2017-03-01 10:53:51 +0800

    按照类图创建TimeSubjec

---
+++ clock/src/main/java/com/gdwii/codeclean/TimeSubject.java
@@ -0,0 +1,17 @@
+package com.gdwii.codeclean;
+
+import java.util.HashMap;
+
+public abstract class TimeSubject {
+    protected Map<String, Clock> clocks = new HashMap<>();
+
+    public void attach(String cityName, Clock clock){
+        clocks.put(cityName, clock);
+    }
+
+    public void detach(String cityName){
+        clocks.remove(cityName);
+    }
+
+    public abstract  void notifyAllClocks();
+}
